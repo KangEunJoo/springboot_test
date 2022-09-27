1da8b44c6212db6def1feaff93aa6de247d0e257
KangEunJoo <kej8640@naver.com>
2022-09-27 15:23:50 +0900

    commit

--- 0916/src/main/java/com/example/demo/student/StudentController.java
+++ 0916/src/main/java/com/example/demo/student/StudentController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path="api/v1/student")
-@CrossOrigin(origins="http://localhost:3000")
+@CrossOrigin(origins="http://localhost:3000",allowCredentials = "true")
 public class StudentController {
 	
 	private final StudentService studentService;
